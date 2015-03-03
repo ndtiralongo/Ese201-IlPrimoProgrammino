f098391f35dea61c6081a024656339eba0649f44
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-03 21:00:49 +0100

    Update maggiorenne.sh

--- maggiorenne.sh
+++ maggiorenne.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+#con "me" indico maggiore eta'
+me=18
+echo -n " inserire eta' persona"
+read ep
+
+if($ep -gt $me )
+echo -n " la persona e' maggiorenne"
+else
+echo -n " la persona e' minorenne"
+fi
+
+done
+
+
+
+
+
+
+
+
+
+
+
+exit 0
