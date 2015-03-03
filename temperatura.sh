8fbcdffcb1f474f845719d714a571060bdf253a0
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-03 21:08:30 +0100

    Update temperatura.sh

--- temperatura.sh
+++ temperatura.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+#Programma che visualizza se la temperatura di una cisterna puo' causare pericolo oppure no
+
+echo -n " inserire temperatura cisterna "
+read t
+t1=18
+t2=5
+
+if($t -gt $t1)
+
+echo -n " situazione normale "
+
+else
+
+if ($t -le $t2)
+echo -n " danni "
+
+else
+echo -n " pericolo"
+fi
+
+
+done	
+
+
+
+
+exit 0	
+
 
