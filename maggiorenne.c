7238f921460799bad07085c186901dd1a258d346
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-03 20:45:08 +0100

    Create maggiorenne.c

---
+++ maggiorenne.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*
+Programma semplice che visualizza se la persona e' maggiorenne
+
+*/
+main()
+{
+      int ep,me;
+      printf(" inserire eta' persona");
+		scanf("%d",&ep);
+		me=18;
+		if(ep>=me)
+		{
+                  printf(" la persona e' maggiorenne");
+                  }
+                  else
+                  {
+                      printf(" la persona e' minorenne");
+                  }
+                  
+      
+      
+      
+      
+    
+      
+printf("\n\n");
+
+}
+   
+           
